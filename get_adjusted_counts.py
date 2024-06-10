9b42cc67974e4ff30f37d92994f2e045d7a1d408
Josh <jmckerra@purdue.edu>
2024-06-10 14:08:38 -0600

    Small updates

--- get_adjusted_counts.py
+++ get_adjusted_counts.py
@@ -2,14 +2,6 @@ import logging
 import time
 from services.score_generator import score_generator
 
-
-# global variables
-backoff_time_in_ms = 180000
-backoff_time_in_seconds = 180
-min_page_count = 12
-missed_authors = set()
-retry_interval_seconds = 600
-
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -22,7 +14,6 @@ def run():
     all_school_scores = score_generator.generate_all_scores()
 
     # get the missed authors data until missed_authors is empty
-    logger.info(f"Missed authors: {missed_authors}")
     score_generator.retry_missed_authors(all_school_scores)
 
     # adds the author count key to all_school_scores
